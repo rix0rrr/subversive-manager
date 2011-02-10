7c1bc19e4e273576273d5d4ba1f80c387e56e79e
rix0rrr <rix0rrr@gmail.com>
2011-02-10 12:09:33 +0100

    Forgot to remove another spurious decode (leading to errors handling dashes in repository names)

--- Subversive/src/test/java/nl/rix0r/subversive/server/ConfigWriterTest.java
+++ Subversive/src/test/java/nl/rix0r/subversive/server/ConfigWriterTest.java
@@ -2,11 +2,13 @@
 package nl.rix0r.subversive.server;
 
 import nl.rix0r.subversive.subversion.Access;
+import nl.rix0r.subversive.subversion.AddUserToGroup;
 import nl.rix0r.subversive.subversion.Directory;
 import nl.rix0r.subversive.subversion.GrantPermission;
 import nl.rix0r.subversive.subversion.Group;
 import nl.rix0r.subversive.subversion.NewGroup;
 import nl.rix0r.subversive.subversion.Permission;
+import nl.rix0r.subversive.subversion.User;
 import org.apache.commons.io.output.StringBuilderWriter;
 import org.junit.Assert;
 import org.junit.Test;
@@ -21,14 +23,31 @@ public class ConfigWriterTest {
 
     @Test
     public void localGroupReference() throws Exception {
-        new NewGroup(new Group("foo", "Owners")).apply(config);
+        String repo = "foo";
+        new NewGroup(new Group(repo, "Owners")).apply(config);
         new GrantPermission(new Permission(
-                new Directory("foo", "/"),
-                new Group("foo", "Owners"),
+                new Directory(repo, "/"),
+                new Group(repo, "Owners"),
                 Access.Read)).apply(config);
         config.save(writer);
 
         Assert.assertTrue(writer.toString().contains("@foo.Owners="));
+    }
+
+    @Test
+    public void writeGroupWithDash() throws Exception {
+        String repo = "foo-bar";
+        Group group = new Group(repo, "Owners");
+        new NewGroup(group).apply(config);
+        new AddUserToGroup(new User("henk"), group).apply(config);
+        new GrantPermission(new Permission(
+                new Directory(repo, "/"),
+                group,
+                Access.Read)).apply(config);
+        config.save(writer);
+
+        Assert.assertTrue("Permission", writer.toString().contains("@foo-bar.Owners=r"));
+        Assert.assertTrue("Group definition", writer.toString().contains("foo-bar.Owners = henk"));
     }
 
 }
